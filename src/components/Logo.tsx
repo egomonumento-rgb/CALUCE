44f13495682d7837950cead4d92dcc66822ee3a2
egomonumento-rgb <egomonumento@gmail.com>
2026-07-02 12:22:28 -0500

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { currentBrand } from '../lib/brand';
 
 interface LogoProps {
   className?: string;
@@ -9,13 +10,13 @@ export const Logo: React.FC<LogoProps> = ({ className = "", showText = true }) =
   return (
     <div className={`flex items-center gap-3 ${className}`}>
       <div className="relative w-10 h-12 flex items-center justify-center">
-        {/* Shadow/Oval below */}
+        {/* Sombra inferior */}
         <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-8 h-2 bg-[#7a92c7] opacity-60 rounded-full" />
         
-        {/* Pin Shape */}
+        {/* Imagen del Logo Dinámica */}
         <img 
-          src="/logo.png" 
-          alt="SeniorTrack Logo" 
+          src={currentBrand.logoUrl} 
+          alt={`${currentBrand.name} Logo`} 
           className="w-full h-full object-contain"
           referrerPolicy="no-referrer"
         />
@@ -23,8 +24,9 @@ export const Logo: React.FC<LogoProps> = ({ className = "", showText = true }) =
       
       {showText && (
         <div className="flex flex-col leading-none">
-          <span className="font-lexend font-bold text-lg text-[#768ec2] -mb-1">senior</span>
-          <span className="font-lexend font-black text-2xl text-[#2f50a3]">track</span>
+          <span className="font-lexend font-black text-xl text-[#2f50a3]">
+            {currentBrand.name}
+          </span>
         </div>
       )}
     </div>
